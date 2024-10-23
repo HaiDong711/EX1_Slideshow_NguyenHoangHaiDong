aec326668c5642fa484b57a72fe9758551d799e5
HaiDong711 <169123281+HaiDong711@users.noreply.github.com>
2024-10-23 09:52:39 +0700

    new

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Slideshow from './component/SlideShow.jsx';
+import Slideshow from './components/Slideshow/Slideshow';
 
 function App() {
   return (
     <div className="App">
-      <h1>Slideshow</h1>
-      <Slideshow />
+      <h1>Slideshow Component</h1>
+      <Slideshow />  {}
     </div>
   );
 }
